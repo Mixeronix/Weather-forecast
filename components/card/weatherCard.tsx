86bd0d05f1c3acb62ceb27a6db32cfdce6014fbe
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-05-22 12:49:27 +0200

    Design changes

--- components/card/weatherCard.tsx
+++ components/card/weatherCard.tsx
@@ -36,7 +36,7 @@ export default function WeatherCard(props: { weatherData: weatherType; index: nu
 		<div
 			className={`aspect-5/6 w-10/12 sm:w-3/4 max-h-11/20 md:max-h-2/3 flex flex-col justify-around pb-32 gap-y-6 md:w-2/3 lg:w-1/2 xl:w-2/5 2xl:w-1/3 absolute top-0 left-0 bottom-0 right-0 m-auto rounded-3xl cardGlassBackground transition-all 
 			${props.index > props.show ? "translate-x-full " : ""} ${props.index < props.show ? "-translate-x-full " : ""} 
-			${props.index == props.show + 1 || props.index == props.show - 1 ? "scale-60 blur-md" : ""} 
+			${props.index == props.show + 1 || props.index == props.show - 1 ? "scale-75 blur-sm" : ""} 
 			${props.index > props.show + 1 || props.index < props.show - 1 ? "scale-0 blur-3xl" : ""}`}
 		>
 			<Data weather={weatherData} displayedHour={displayedHour} mainView={props.index == props.show} />
